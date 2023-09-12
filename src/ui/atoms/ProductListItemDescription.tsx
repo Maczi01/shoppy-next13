a23da3cebfae07d982c4459a90117410833bccd9
maczi01 <mateusz.w.twardy@gmail.com>
2023-09-12 23:40:55 +0100

    Created ProductListItemDescription

---
+++ src/ui/atoms/ProductListItemDescription.tsx
@@ -0,0 +1,25 @@
+type ProductListItemDescriptionProps = {
+	product: {
+		name: string;
+		category: string;
+		price: number;
+	};
+};
+
+export const ProductListItemDescription = ({
+	product: { name, price, category },
+}: ProductListItemDescriptionProps) => {
+	return (
+		<div className="mt-2 flex justify-between">
+			<div>
+				<h3 className="text-sm text-gray-700">{name}</h3>
+				<p className="text-sm text-gray-500">
+					<span className="sr-only">Kategoria</span> {category}$
+				</p>
+			</div>
+			<p className="text-sm text-gray-900">
+				<span className="sr-only">Cena</span> {price}$
+			</p>
+		</div>
+	);
+};
