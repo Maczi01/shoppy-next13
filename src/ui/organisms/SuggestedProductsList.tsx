664a517d413c171322a8a65a01b0bf08ce052186
maczi01 <mateusz.w.twardy@gmail.com>
2023-09-30 21:31:14 +0100

    Removed sleep func

--- src/ui/organisms/SuggestedProductsList.tsx
+++ src/ui/organisms/SuggestedProductsList.tsx
@@ -1,9 +1,7 @@
 import { ProductList } from "@/ui/organisms/ProductList";
 import { getProductsList } from "@/api/products";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export const SuggestedProductsList = async () => {
 	const products = await getProductsList();
-	await sleep(3100);
 	return <ProductList products={products.slice(-4)} />;
 };
