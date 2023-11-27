118d836233c0178a2f7f99535d1c9b35f20f5dc1
maczi01 <mateusz.w.twardy@gmail.com>
2023-11-27 13:41:53 +0000

    Changed img to Next image element

--- src/ui/components/ProductCoverImage.tsx
+++ src/ui/components/ProductCoverImage.tsx
@@ -1,8 +1,10 @@
+import NextImage from "next/image";
+
 export const ProductCoverImage = ({ alt, src }: { src: string; alt: string }) => {
 	return (
 		<div className="aspect-square overflow-hidden rounded-md border bg-slate-50 hover:bg-slate-100">
-			<img
-				width={100}
+			<NextImage
+				width={200}
 				height={100}
 				alt={alt}
 				src={src}
